de9674b34e8fcc97810c872782a56d206ee6d353
Thiago Prado <tgprado@thoughtworks.com>
2020-03-26 20:41:52 -0300

    Expose javac plugin to service loader

--- src/main/java/ristretto/compiler/plugin/JavacPlugin.java
+++ src/main/java/ristretto/compiler/plugin/JavacPlugin.java
@@ -2,6 +2,9 @@ package ristretto.compiler.plugin;
 
 import com.sun.source.util.JavacTask;
 import com.sun.source.util.Plugin;
+import com.sun.tools.javac.api.BasicJavacTask;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.Log;
 
 public final class JavacPlugin implements Plugin {
 
@@ -12,6 +15,7 @@ public final class JavacPlugin implements Plugin {
 
     @Override
     public void init(final JavacTask task, final String... args) {
-
+        Context context = ((BasicJavacTask) task).getContext();
+        Log.instance(context).printRawLines(Log.WriterKind.NOTICE, getName() + " plugin");
     }
 }
