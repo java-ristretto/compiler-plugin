603f656d93a47ed871406177f4c4bc3e8adfcb14
Thiago Prado <tgprado@thoughtworks.com>
2020-03-27 13:30:13 -0300

    Scan for all method parameters and set them as 'final'

--- src/main/java/ristretto/compiler/plugin/JavacPlugin.java
+++ src/main/java/ristretto/compiler/plugin/JavacPlugin.java
@@ -1,21 +1,58 @@
 package ristretto.compiler.plugin;
 
+import com.sun.source.tree.MethodTree;
 import com.sun.source.util.JavacTask;
 import com.sun.source.util.Plugin;
+import com.sun.source.util.TaskEvent;
+import com.sun.source.util.TaskListener;
+import com.sun.source.util.TreeScanner;
 import com.sun.tools.javac.api.BasicJavacTask;
+import com.sun.tools.javac.code.Flags;
+import com.sun.tools.javac.tree.JCTree.JCVariableDecl;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.Log;
 
 public final class JavacPlugin implements Plugin {
 
+    public static final String NAME = "ristretto";
+
     @Override
     public String getName() {
-        return "ristretto";
+        return NAME;
     }
 
     @Override
-    public void init(final JavacTask task, final String... args) {
+    public void init(JavacTask task, String... args) {
+        task.addTaskListener(OnParseFinished.INSTANCE);
+
         Context context = ((BasicJavacTask) task).getContext();
-        Log.instance(context).printRawLines(Log.WriterKind.NOTICE, getName() + " plugin");
+        Log.instance(context).printRawLines(Log.WriterKind.NOTICE, String.format("%s plugin loaded", NAME));
+    }
+
+    private static final class OnParseFinished implements TaskListener {
+
+        private static final TaskListener INSTANCE = new OnParseFinished();
+
+        @Override
+        public void finished(TaskEvent event) {
+            if (!TaskEvent.Kind.PARSE.equals(event.getKind())) {
+                return;
+            }
+            event.getCompilationUnit().accept(MethodParameterFinalModifier.INSTANCE, null);
+        }
+    }
+
+    private static final class MethodParameterFinalModifier extends TreeScanner<Void, Void> {
+
+        private static final TreeScanner<Void, Void> INSTANCE = new MethodParameterFinalModifier();
+
+        @Override
+        public Void visitMethod(MethodTree method, Void data) {
+            for (var parameter : method.getParameters()) {
+                JCVariableDecl declaration = (JCVariableDecl) parameter;
+                declaration.mods.flags |= Flags.FINAL;
+            }
+            return super.visitMethod(method, data);
+        }
     }
 }
--- src/test/java/ristretto/compiler/plugin/JavacPluginTest.java
+++ src/test/java/ristretto/compiler/plugin/JavacPluginTest.java
@@ -1,16 +1,46 @@
 package ristretto.compiler.plugin;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.containsString;
 
 class JavacPluginTest {
 
-    @Test
-    void has_name() {
-        JavacPlugin plugin = new JavacPlugin();
+    private TestCompiler compiler;
 
-        assertThat(plugin.getName(), is("ristretto"));
+    @BeforeEach
+    void beforeEach() {
+        compiler = TestCompiler.newInstance();
+    }
+
+    @Test
+    void indicates_when_it_is_loaded() {
+        var anEmptyClass = "" +
+            "package ristretto.test;" +
+            "" +
+            "public class TestSample {}";
+
+        var result = compiler.compile(anEmptyClass);
+
+        assertThat(result.additionalOutput, containsString("ristretto plugin loaded"));
+    }
+
+    @Test
+    void enforces_method_parameters_to_be_final() {
+        var mutableParameter = "" +
+            "package ristretto.test;" +
+            "" +
+            "public class TestSample {" +
+            "  public static String hello(String name) {" +
+            "    name = \"hello \" + name;" +
+            "    return name;" +
+            "  }" +
+            "}";
+
+        var result = compiler.compile(mutableParameter);
+
+        assertThat(result.diagnostics(), containsString("TestSample.java:1: error: final parameter name may not be assigned"));
     }
 }
