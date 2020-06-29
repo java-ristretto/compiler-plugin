9c3b1f8da3ea1e4485b5ec60454ac0bcd470a3e6
Thiago Prado <thiago.gozzi.prado@gmail.com>
2020-06-29 13:40:49 -0300

    WIP

---
+++ plugins/intellij/src/main/java/ristretto/intellij/plugin/RistrettoAugmentProvider.java
@@ -0,0 +1,38 @@
+package ristretto.intellij.plugin;
+
+import com.intellij.psi.PsiLocalVariable;
+import com.intellij.psi.PsiModifier;
+import com.intellij.psi.PsiModifierList;
+import com.intellij.psi.augment.PsiAugmentProvider;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
+public final class RistrettoAugmentProvider extends PsiAugmentProvider {
+
+    private static final Logger LOG = Logger.getLogger(RistrettoAugmentProvider.class.getName());
+
+    public RistrettoAugmentProvider() {
+        LOG.info("ristretto augment provider loaded");
+    }
+
+    static interface X {
+
+    }
+
+    @Override
+    protected @NotNull Set<String> transformModifiers(
+        @NotNull PsiModifierList modifierList,
+        @NotNull Set<String> modifiers
+    ) {
+        if (modifierList.getParent() instanceof PsiLocalVariable) {
+            Set<String> result = new HashSet<>(modifiers);
+            result.add(PsiModifier.FINAL);
+            return result;
+        }
+
+        return modifiers;
+    }
+}
